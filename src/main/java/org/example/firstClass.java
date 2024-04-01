16ce4d998c721487cdd3847145452ac631dcdac0
ggiridhar1 <ggiridhar91@gmail.com>
2024-04-01 22:28:04 +0530

    second commit

--- src/main/java/org/example/firstClass.java
+++ src/main/java/org/example/firstClass.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -24,13 +25,58 @@ public class firstClass {
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3a");
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-		capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\girid\\Downloads\\ApiDemos.apk");
+		capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/girid/OneDrive/Documents/apks/9.3.0-qarelease-v9.2.3-D202311060923 (1).apk");
 
 		driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
 		Thread.sleep(10000);
 
-		WebElement asseibilityButton = driver.findElementByXPath("//android.widget.TextView[@content-desc='App']");
+		 //driver.findElementByXPath("//android.widget.TextView[@content-desc='App']");
+		WebElement elements = driver.findElementByXPath("//android.widget.EditText[@content-desc='login-phNumber-tb']");
+
+		//touch
+		TouchAction touchAction = new TouchAction(driver);
+		touchAction.tap(elements).perform();
+
+		//send keys
+		elements.sendKeys("9972311465");
+
+		//tap on sign in with otp
+		WebElement otp=driver.findElementByXPath("//android.widget.TextView[@text='Sign in with OTP']");
+		touchAction.tap(otp).perform();
+
+		Thread.sleep(1000);
+
+		//enter otp & verify Otp
+		WebElement enterOtp=driver.findElementByAccessibilityId("login-otp-tb");
+		enterOtp.sendKeys("123456");
+		WebElement verfyOtp=driver.findElementByAccessibilityId("login-verify-btn");
+		touchAction.tap(verfyOtp).perform();
+
+		Thread.sleep(1000);
+
+		//Relational type
+		WebElement type = driver.findElementByXPath("//android.view.ViewGroup[@content-desc='ws-enterprise-tile']");
+		touchAction.tap(type).perform();
+
+		Thread.sleep(1000);
+
+		//work space
+		WebElement workspace = driver.findElementByXPath("//android.view.ViewGroup[@content-desc='ws-customer-tile']");
+		touchAction.tap(workspace).perform();
+
+		Thread.sleep(1000);
+
+		//Customer select
+		WebElement customerSelect =driver.findElementByAccessibilityId("Customer, tab, 2 of 4");
+		touchAction.tap(customerSelect).perform();
+
+		Thread.sleep(10000);
+		driver.quit();
+
+
+
+
 
 	}
 }
